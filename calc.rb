aa172328acf87dd4595814a6dafc5e58cae06422
vafmoraes <112376376+vafmoraes@users.noreply.github.com>
2022-10-08 22:55:28 -0300

    Estudando

---
+++ calc.rb
@@ -0,0 +1,9 @@
+print "Digite o primeiro numero inteiro: "
+# .to_i transforma a string em um numero inteiro
+number1 = gets.chomp.to_i
+
+print "Digite o segundo numero inteiro: "
+number2 = gets.chomp.to_i
+
+addition = number1 + number2
+puts "O resultado da adiçao entre os dois números é #{addition}"
---
+++ dados.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+addition = nome + idade
+puts "Nome e idade #{addition}"
